241b5abbe94e294303a1cfb96b527028a6c14484
Dzaky Nashshar <97614270+dzackiero@users.noreply.github.com>
2025-08-29 19:25:15 +0700

    Refactor SupabaseStrategy for JWT handling
    Refactor SupabaseStrategy to improve JWT handling and remove unnecessary code.

--- src/auth/strategies/supabase.strategy.ts
+++ src/auth/strategies/supabase.strategy.ts
@@ -1,18 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { Request } from 'express';
-import { ParamsDictionary } from 'express-serve-static-core';
-import { ParsedQs } from 'qs';
 
 @Injectable()
-export class SupabaseStrategy extends PassportStrategy(Strategy) {
-  public constructor(private readonly configService: ConfigService) {
+export class SupabaseStrategy extends PassportStrategy(Strategy, 'supabase') {
+  constructor(configService: ConfigService) {
     const jwtSecret = configService.get<string>('JWT_SECRET');
+
     if (!jwtSecret) {
       throw new Error('JWT_SECRET is not defined in environment variables');
     }
@@ -20,17 +15,11 @@ export class SupabaseStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET'),
+      secretOrKey: jwtSecret, // ✅ now always string
     });
   }
 
-  async validate(payload: any): Promise<any> {
-    return await payload;
-  }
-
-  authenticate(
-    req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
-  ) {
-    super.authenticate(req);
+  async validate(payload: any) {
+    return payload;
   }
 }
